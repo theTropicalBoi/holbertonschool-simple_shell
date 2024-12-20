377f79e53f6ba3b0c6ce127f4f1af062364281a6
theTropicalBoi <shaans1290@icloud.com>
2024-12-20 10:41:34 +0100

    Clean main.h

--- main.h
+++ main.h
@@ -4,10 +4,10 @@
 /* Library: */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /* Function Prototype: */
 void shell_interpreter(void);

f424d30d1b9c818c69d8fcc49601663153dd30dd
YANN11GAB <danieloubassou@gmail.com>
2024-12-20 11:10:36 +0100

    prompt

--- interpreter.c
+++ interpreter.c
@@ -1,10 +1,20 @@
 #include "main.h"
 /**
- * shell_interpreter - 
+ * shell_interpreter - main block
+ * Return: 0
  */
 void shell_interpreter(void)
 {
-	printf("$ >")
+        size_t n = 0;
+        char *buf = NULL;
+
+        printf("$");
+        getline(&buf, &n, stdin);
+        printf("%s", buf);
+        printf("Buffer size: %ld\n", n);
+
+        free(buf);
+        return (0);
 }
 
 /**
